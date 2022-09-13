4625d3b0fbf7671fe3575072cbcdb54816ff8b6c
s-triar <34316314+s-triar@users.noreply.github.com>
2022-09-13 20:11:09 +0700

    aaa

--- src/app/api-services/plant.service.ts
+++ src/app/api-services/plant.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { PlantMockService } from '../mock-services/plant-mock.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
+  useFactory: (p: any[], h: HttpClient, u: UtilityService) => {
+    if (p[0] === true) {
+      return new PlantMockService();
+    } else {
+      return new PlantService(h, u);
+    }
+  },
+  deps: ['mocking', HttpClient, UtilityService],
 })
 export class PlantService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private utilityService: UtilityService
+  ) {}
 }
