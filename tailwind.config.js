e9080aa11fbca6d49f3bba775f4902b48f504142
Daffa404 <58094200+Daffa404@users.noreply.github.com>
2022-11-07 15:58:07 +0700

    tw --

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
           'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
           'Noto Color Emoji'
         ]
+      },
+      colors: {
+        'blue':'#0C9DA4',
+        'yellow' :'#F89747',
+        'red' :'#BB2026',
+        'green' :'#07A04A',
+        'white-221': '#ddd'
       }
     },
   },
